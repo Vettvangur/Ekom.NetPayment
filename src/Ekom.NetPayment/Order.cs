36e32d729fc76e9df5e1fc9cd85b5f82456b4473
Gunnar Már Óttarsson <gunni@starcraft.is>
2016-10-06 12:53:32 +0000

    First

---
+++ src/Ekom.NetPayment/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Persistence;
+
+namespace Umbraco.NetPayment
+{
+    [TableName("customOrder")]
+    [PrimaryKey("id", autoIncrement = false)]
+    public class Order
+    {
+        public Guid id { get; set; }
+
+        public int member { get; set; }
+
+        public decimal amount { get; set; }
+
+        public DateTime date { get; set; }
+
+        public bool paid { get; set; }
+
+        public string paymentProvider { get; set; }
+    }
+}
---
+++ src/Ekom.NetPayment/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umbraco.NetPayment
+{
+    public class OrderItem
+    {
+        public decimal GrandTotal { get; set; }
+
+        public int Price { get; set; }
+
+        public int Discount { get; set; }
+
+        public string Title { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
