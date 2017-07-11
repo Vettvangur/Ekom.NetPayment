9fa90b83945323cdb0e3b18dad090a61bb0dd072
Gunnar Már Óttarsson <gunni@starcraft.is>
2017-07-11 21:14:58 +0000

    More work on refactoring

---
+++ src/Ekom.NetPayment/Settings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umbraco.NetPayment
+{
+    /// <summary>
+    /// Various settings for the Umbraco.NetPayment package.
+    /// Most settings can be overriden in AppSettings.
+    /// </summary>
+    public class Settings
+    {
+        private string _ppConfigPath = null;
+        /// <summary>
+        /// Path to payment provider configuration.
+        /// That file stores payment provider specific information that often gets xml transformed.
+        /// F.x. merchantId and secret
+        /// </summary>
+        public virtual string PPConfigPath
+        {
+            get
+            {
+                if (_ppConfigPath == null)
+                {
+                    var ppConfigPath = ConfigurationManager.AppSettings["NetPayment.PPConfigPath"];
+
+                    _ppConfigPath = ppConfigPath ?? "~/App_Plugins/NetPayment/config/PaymentProviders.config";
+                }
+
+                return _ppConfigPath;
+            }
+        }
+    }
+}
