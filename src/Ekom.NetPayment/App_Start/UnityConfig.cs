d27c272a94446f1a442407ae2b11ea7ce824a50a
Gunnar Már Óttarsson <gunni@starcraft.is>
2017-09-14 11:11:34 +0000

    Simplify Settings unity config

--- src/Ekom.NetPayment/App_Start/UnityConfig.cs
+++ src/Ekom.NetPayment/App_Start/UnityConfig.cs
@@ -58,7 +58,7 @@ namespace Umbraco.NetPayment
                         container.Resolve<HttpContextBase>().Server
             ));
 
-            container.RegisterType<Settings>(new ContainerControlledLifetimeManager(), new InjectionFactory(c => new Settings()));
+            container.RegisterType<Settings>(new ContainerControlledLifetimeManager());
             container.RegisterType<ApplicationContext>(new ContainerControlledLifetimeManager(), new InjectionFactory(c => ApplicationContext.Current));
             container.RegisterType<UmbracoConfig>(new ContainerControlledLifetimeManager(), new InjectionFactory(c => UmbracoConfig.For));
 
