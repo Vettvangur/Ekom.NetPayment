356ef60094d30ac2da219c7bc2299a7f2490f9d0
Gunnar Már Óttarsson <gunni@starcraft.is>
2018-02-01 23:05:40 +0000

    TinyIoC configuration complete

--- src/Ekom.NetPayment/App_Start/TinyIoCConfig.cs
+++ src/Ekom.NetPayment/App_Start/TinyIoCConfig.cs
@@ -17,18 +17,18 @@ namespace Umbraco.NetPayment
         /// <param name="container">The TinyIoC container to configure.</param>
         public static void RegisterTypes(TinyIoCContainer container)
         {
-            container.Register<HttpContextBase>((c, p) => new HttpContextWrapper(HttpContext.Current)).AsPerRequestSingleton();
-            container.Register<HttpRequestBase>((c, p) => c.Resolve<HttpContextBase>().Request).AsPerRequestSingleton();
-            container.Register<HttpResponseBase>((c, p) => c.Resolve<HttpContextBase>().Response).AsPerRequestSingleton();
-            container.Register<HttpServerUtilityBase>((c, p) => c.Resolve<HttpContextBase>().Server).AsPerRequestSingleton();
+            container.Register<HttpContextBase>((c, p) => new HttpContextWrapper(HttpContext.Current));
+            container.Register<HttpRequestBase>((c, p) => c.Resolve<HttpContextBase>().Request);
+            container.Register<HttpResponseBase>((c, p) => c.Resolve<HttpContextBase>().Response);
+            container.Register<HttpServerUtilityBase>((c, p) => c.Resolve<HttpContextBase>().Server);
 
             container.Register<Settings>().AsSingleton();
-            container.Register<ApplicationContext>((c, p) => ApplicationContext.Current).AsSingleton();
-            container.Register<ExamineManagerBase>((c, p) => new ExamineManagerWrapper(ExamineManager.Instance)).AsSingleton();
-            container.Register<UmbracoConfig>((c, p) => UmbracoConfig.For).AsSingleton();
+            container.Register<ApplicationContext>((c, p) => ApplicationContext.Current);
+            container.Register<ExamineManagerBase>((c, p) => new ExamineManagerWrapper(ExamineManager.Instance));
+            container.Register<UmbracoConfig>((c, p) => UmbracoConfig.For);
 
-            container.Register<UmbracoContext>((c, p) => UmbracoContext.Current).AsPerRequestSingleton();
-            container.Register<UmbracoHelper>((c, p) => new UmbracoHelper(c.Resolve<UmbracoContext>())).AsPerRequestSingleton();
+            container.Register<UmbracoContext>((c, p) => UmbracoContext.Current);
+            container.Register<UmbracoHelper>((c, p) => new UmbracoHelper(c.Resolve<UmbracoContext>()));
 
             container.Register<IFileSystem, FileSystem>().AsMultiInstance();
             container.Register<IDatabaseFactory, DatabaseFactory>().AsMultiInstance();
