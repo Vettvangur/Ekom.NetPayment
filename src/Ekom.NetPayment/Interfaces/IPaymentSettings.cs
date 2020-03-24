8d1b7c763e17c6f4801b1a6df5a8921a77b83e87
Gunnar Már Óttarsson <gunni@starcraft.is>
2020-03-24 16:28:09 +0000

    SiminnPay minor

--- src/Ekom.NetPayment/Interfaces/IPaymentSettings.cs
+++ src/Ekom.NetPayment/Interfaces/IPaymentSettings.cs
@@ -142,8 +142,13 @@ namespace Umbraco.NetPayment
 
         /// <summary>
         /// Displayed in SiminnPay payment overview
+        /// This value facilitates correlating a orderid from the merchants system (f.x. Ekom orderid) 
+        /// with an order displayed in the payment authorizers system.
+        /// 
         /// We cannot support this in f.x. Valitor since the reference is used by netPayment
         /// to find it's order and related data in callback.
+        /// That is, the value sent to Valitor as the reference must always match 
+        /// the netpayment orderid, therefore we cannot allow a custom reference to be provided.
         /// 
         /// Supported by: SiminnPay
         /// </summary>
