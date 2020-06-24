649008e4c5b795a23efeb52bc90af8352b8b52ed
Gunnar Már Óttarsson <gunni@starcraft.is>
2020-06-24 18:08:25 +0000

    Minor nuspec and buildscript

--- release-build-push.ps1
+++ release-build-push.ps1
@@ -1,4 +1,4 @@
 nuget restore .\src
-nuget pack .\src\Ekom.NetPayment\ -build -Symbols -SymbolPackageFormat snupkg
+nuget pack .\src\Ekom.NetPayment\ -build -Symbols -SymbolPackageFormat snupkg -Properties Configuration=Release
 # $pkg = gci *.nupkg 
 # nuget push $pkg -Source https://www.nuget.org/api/v2/package -NonInteractive
